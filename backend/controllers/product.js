f1d421ab5944eabbebd5f8205797c06ba2ed817b
dev <dev@gmail.com>
2021-10-25 12:42:30 +0530

    Product CRud done

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -1,12 +1,48 @@
-const { successResponse } = require("../helper/response");
+const { successResponse, errorResponse } = require("../helper/response");
 const Product = require("../models/product");
 
-exports.getAllProducts = (req, res) => {
-    res.status(200).json({message: "Route is working fine"});
+//All Product
+exports.getAllProducts = async (req, res) => {
+    
+    const product = await Product.find();
+    return successResponse(res, product);
 }
 
+//Create Product
 exports.createProduct = async (req, res) => {
 
     const product = await Product.create(req.body);
-    successResponse(res, product, 201);
+    return successResponse(res, product, 201);
+}
+
+//Update Product
+exports.updateProduct = async (req, res) => {
+
+    const product = await Product.findById(req.params.id);
+
+    if(!product) {
+        return errorResponse(res, { message: "No product found " });
+    }
+
+    const result = await Product.findOneAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    })
+
+    return successResponse(res, result);
+}
+
+//Delete Product
+exports.deleteProduct = async (req, res) => {
+
+    const product = await Product.findById(req.params.id);
+
+    if(!product) {
+        return errorResponse(res, { message: "No product found " });
+    }
+
+    product.remove();
+
+    return successResponse(res);
 }
