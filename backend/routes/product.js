c3ec3d539eba85a1a860f073d738283ad742c047
dev <dev@gmail.com>
2021-10-30 10:02:38 +0530

    chnage route of product according to admin

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -5,7 +5,8 @@ const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 const router = express.Router();
 
 router.route("/").get(isAuthenticatedUser, authorizeRoles("admin"), getAllProducts);
-router.route("/").post(createProduct);
-router.route("/:id").put(updateProduct).delete(deleteProduct).get(productDetail);
+router.route("/admin").post(authorizeRoles("admin"), createProduct);
+router.route("/:id/admin").put(authorizeRoles("admin"), updateProduct).delete(authorizeRoles("admin"), deleteProduct);
+router.route("/:id").get(productDetail);
 
 module.exports = router;
