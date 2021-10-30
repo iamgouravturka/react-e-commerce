63c0b66286c75fc430c47b6f76d58e9957007fe6
dev <dev@gmail.com>
2021-10-30 10:10:46 +0530

    admin get all user and get user detail api done

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const { register, login, logout, forgotPassword, resetPassword, getMyProfile, updatePassword, updateProfile } = require("../controllers/user");
-const { isAuthenticatedUser } = require("../middleware/auth");
+const { register, login, logout, forgotPassword, resetPassword, getMyProfile, updatePassword, updateProfile, getUserDetail } = require("../controllers/user");
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 const router = express.Router();
 
@@ -13,5 +13,8 @@ router.route("/logout").get(logout);
 router.route("/").get(isAuthenticatedUser, getMyProfile);
 router.route("/updatePassword").get(isAuthenticatedUser, updatePassword);
 router.route("/").get(isAuthenticatedUser, updateProfile);
+
+router.route("/:id/admin").get(authorizeRoles("admin"), isAuthenticatedUser, getUserDetail);
+router.route("/admin").get(authorizeRoles("admin"), isAuthenticatedUser, getAllUsers);
 
 module.exports = router;
