ded7bb531e0eb695ae5e9bd579fe9a821241b491
dev <dev@gmail.com>
2021-10-30 15:49:12 +0530

    my orders api done

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const { newOrder } = require("../controllers/order");
+const { newOrder, getOrderDetail, myOrder } = require("../controllers/order");
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 const router = express.Router();
 
-router.route("/").post(isAuthenticatedUser, newOrder);
-router.route("/:").post(isAuthenticatedUser, getOrder);
+router.route("/").post(isAuthenticatedUser, authorizeRoles("admin"), newOrder).get(isAuthenticatedUser, authorizeRoles("admin"), myOrder);
+router.route("/:id").post(isAuthenticatedUser, authorizeRoles("admin"), getOrderDetail);
 
 module.exports = router;
