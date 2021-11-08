178a56f6ce47539c94fb7d13b0f49d53036b5cbc
dev <dev@gmail.com>
2021-11-08 12:18:36 +0530

    delete order done

--- backend/controllers/order.js
+++ backend/controllers/order.js
@@ -78,3 +78,18 @@ exports.updateOrder = catchAsyncErrors(async (req, res, next) => {
   
     await product.save({ validateBeforeSave: false });
   }
+
+// delete Order -- Admin
+exports.deleteOrder = catchAsyncErrors(async (req, res, next) => {
+    const order = await Order.findById(req.params.id);
+  
+    if (!order) {
+      return next(new ErrorHander("Order not found with this Id", 404));
+    }
+  
+    await order.remove();
+  
+    res.status(200).json({
+      success: true,
+    });
+  });
